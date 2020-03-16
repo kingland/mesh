ff227f8d3c24450a2914dc2667fadb0e73f0bf08
Johannes Schüth <git@jotschi.de>
2020-03-16 13:21:47 +0100

    Add toplogy lock timeout for server startup

--- server/src/main/java/com/gentics/mesh/server/cluster/ClusterServer.java
+++ server/src/main/java/com/gentics/mesh/server/cluster/ClusterServer.java
@@ -5,7 +5,6 @@ import com.gentics.mesh.OptionsLoader;
 import com.gentics.mesh.context.impl.LoggingConfigurator;
 import com.gentics.mesh.dagger.MeshComponent;
 import com.gentics.mesh.etc.config.MeshOptions;
-import com.gentics.mesh.etc.config.cluster.CoordinatorMode;
 import com.gentics.mesh.router.EndpointRegistry;
 import com.gentics.mesh.verticle.admin.AdminGUI2Endpoint;
 import com.gentics.mesh.verticle.admin.AdminGUIEndpoint;
@@ -23,21 +22,24 @@ public class ClusterServer {
 		// options.getHttpServerOptions().setCorsAllowCredentials(true);
 		// options.getHttpServerOptions().setEnableCors(true);
 		// options.getHttpServerOptions().setCorsAllowedOriginPattern("http://localhost:5000");
-
 		options.getAuthenticationOptions().setKeystorePassword("finger");
 		options.setInitialAdminPassword("admin");
 		options.setForceInitialAdminPasswordReset(false);
 
 		//options.getClusterOptions().setCoordinatorMode(CoordinatorMode.ALL);
 		//options.getClusterOptions().setCoordinatorRegex("gentics-mesh-[0-9]");
-		options.getStorageOptions().setSynchronizeWrites(true);
 		options.getStorageOptions().setStartServer(true);
 		options.getClusterOptions().setClusterName("test");
 		options.getClusterOptions().setEnabled(true);
 		options.getSearchOptions().setUrl(null);
 		options.getSearchOptions().setStartEmbedded(false);
-		options.getClusterOptions().setTopologyLockTimeout(240_000);
 		options.getDebugInfoOptions().setLogEnabled(false);
+
+		// New settings
+		options.getStorageOptions().setSynchronizeWrites(true);
+		options.getStorageOptions().setSynchronizeWritesTimeout(90_000);
+		options.getClusterOptions().setTopologyLockTimeout(240_000);
+		options.getClusterOptions().setTopologyLockDelay(20_000);
 		return options;
 	}
 
